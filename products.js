43f4af41d4fd81e2800ff489bc5a2b46c1a58486
Armita Koleini <104152352+ArmitaKoleini@users.noreply.github.com>
2023-10-21 23:09:35 +0330

    Add files via upload

---
+++ products.js
@@ -0,0 +1,50 @@
+export const productsData = [
+  {
+    id: 1,
+    title: "Product 001",
+    price: 10.99,
+    imageUrl: "./images/product-1.jpg",
+  },
+  {
+    id: 2,
+    title: "Product 002",
+    price: 14.99,
+    imageUrl: "./images/product-2.jpg",
+  },
+  {
+    id: 3,
+    title: "Product 003",
+    price: 7.99,
+    imageUrl: "./images/product-3.jpg",
+  },
+  {
+    id: 4,
+    title: "Product 004",
+    price: 11.99,
+    imageUrl: "./images/product-4.jpg",
+  },
+  {
+    id: 5,
+    title: "Product 005",
+    price: 4.99,
+    imageUrl: "./images/product-5.jpg",
+  },
+  {
+    id: 6,
+    title: "Product 006",
+    price: 23.99,
+    imageUrl: "./images/product-6.jpg",
+  },
+  {
+    id: 7,
+    title: "Product 007",
+    price: 42.99,
+    imageUrl: "./images/product-7.jpg",
+  },
+  {
+    id: 8,
+    title: "Product 008",
+    price: 24.99,
+    imageUrl: "./images/product-8.jpg",
+  },
+];
